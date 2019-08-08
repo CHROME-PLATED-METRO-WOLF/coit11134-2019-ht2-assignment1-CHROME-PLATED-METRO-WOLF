3ab57ed66bc54c3d5784e9440937a3b610661ab5
caleb <caleb.davidson@cqumail.com>
2019-08-08 17:27:02 +1000

    Updated comments.

--- src/user.java
+++ src/user.java
@@ -9,15 +9,7 @@ import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author caleb
- */
+
 public class user {
 //private variables for user class
     private String userName;
