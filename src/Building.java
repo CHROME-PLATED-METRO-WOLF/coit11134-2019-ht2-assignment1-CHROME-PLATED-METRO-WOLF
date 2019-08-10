96a54be6157ca1504dde49efe3a86c6b3713738e
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2019-08-10 18:07:43 +1000

    Added default constructor. Added comments.

--- src/Building.java
+++ src/Building.java
@@ -11,9 +11,34 @@ public class Building {
     private String state;
 
     //less basic data
+    
+    //if the building has a name or is asociated with a business
+    //example empire state building
+    //or fbi headquarters
+    private String buildingName;
     private int rooms;
     private String notes;
     //Building type apartment, house, commercial, industrial ect. 
     private String buildingType;
 
+    
+    //constructors
+    
+    //Default constructor
+    Building()
+    {
+        //creates a blank building object
+        unitNumber = 0;
+        address = "";
+        postcode = 0;
+        suburb = "";
+        city = "";
+        state = "";
+        buildingName = "";
+        rooms = 0;
+        notes = "";
+        buildingType = "";
+    }
+    //no parameterised constructor will be created because data can be entered in any order
+    
 }
