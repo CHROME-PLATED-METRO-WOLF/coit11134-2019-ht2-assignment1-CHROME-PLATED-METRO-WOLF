ecc038ed02e1ca405eaa5f4b0922cdd7af6e8754
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2019-08-10 17:46:32 +1000

    Added more data

--- src/Building.java
+++ src/Building.java
@@ -3,9 +3,16 @@
 public class Building {
    //Data assumes australia as the country
    //Basic data for a building
+   int unitNumber;
    String address;
    int postcode;
    String suburb;
    String city;
    String state;
+   
+   //less basic data
+   int rooms;
+   int zones;
+   String notes;
+   
 }
