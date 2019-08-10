32fd7a3bc12a4a11e7bad59df8a979b872934600
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2019-08-10 17:12:09 +1000

    Building class added. Basic data regarding the building added. Basic comments.

---
+++ src/Building.java
@@ -0,0 +1,11 @@
+
+//building can be added without installation (pre inspection)
+public class Building {
+   //Data assumes australia as the country
+   //Basic data for a building
+   String address;
+   int postcode;
+   String suburb;
+   String city;
+   String state;
+}
