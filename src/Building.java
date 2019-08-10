24a85fadfbb4c53111aaf07b5af930026b2543a0
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2019-08-10 17:49:14 +1000

    Removed zones.

--- src/Building.java
+++ src/Building.java
@@ -1,18 +1,17 @@
 
 //building can be added without installation (pre inspection)
 public class Building {
-   //Data assumes australia as the country
-   //Basic data for a building
-   private int unitNumber;
-   private String address;
-   private int postcode;
-   private String suburb;
-   private String city;
-   private String state;
-   
-   //less basic data
-   private int rooms;
-   private int zones;
-   private String notes;
-   
+    //Data assumes australia as the country
+    //Basic data for a building
+    private int unitNumber;
+    private String address;
+    private int postcode;
+    private String suburb;
+    private String city;
+    private String state;
+
+    //less basic data
+    private int rooms;
+    private String notes;
+
 }
