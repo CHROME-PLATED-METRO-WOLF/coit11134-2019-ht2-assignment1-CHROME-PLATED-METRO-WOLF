b382f8bc0555846e630d337cdb9061e472918bab
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2019-08-14 16:34:56 +1000

    Format code.

--- src/Building.java
+++ src/Building.java
@@ -1,6 +1,7 @@
 
 //building can be added without installation (pre inspection)
 public class Building {
+
     //Data assumes australia as the country
     //Basic data for a building
     private int unitNumber;
@@ -11,7 +12,6 @@ public class Building {
     private String state;
 
     //less basic data
-    
     //if the building has a name or is asociated with a business
     //example empire state building
     //or fbi headquarters
@@ -21,12 +21,9 @@ public class Building {
     //Building type apartment, house, commercial, industrial ect. 
     private String buildingType;
 
-    
     //constructors
-    
     //Default constructor
-    Building()
-    {
+    Building() {
         //creates a blank building object
         unitNumber = 0;
         address = "";
@@ -39,8 +36,8 @@ public class Building {
         notes = "";
         buildingType = "";
     }
-    Building(int UnitNumber)
-    {
+
+    Building(int UnitNumber) {
         unitNumber = UnitNumber;
         address = "";
         postcode = 0;
@@ -52,8 +49,8 @@ public class Building {
         notes = "";
         buildingType = "";
     }
-    Building(int UnitNumber, String Address)
-    {
+
+    Building(int UnitNumber, String Address) {
         unitNumber = UnitNumber;
         address = Address;
         postcode = 0;
@@ -65,8 +62,8 @@ public class Building {
         notes = "";
         buildingType = "";
     }
-    Building(int UnitNumber, String Address, int PostCode)
-    {
+
+    Building(int UnitNumber, String Address, int PostCode) {
         unitNumber = UnitNumber;
         address = Address;
         postcode = PostCode;
@@ -78,8 +75,8 @@ public class Building {
         notes = "";
         buildingType = "";
     }
-    Building(int UnitNumber, String Address, int PostCode, String Suburb)
-    {
+
+    Building(int UnitNumber, String Address, int PostCode, String Suburb) {
         unitNumber = UnitNumber;
         address = Address;
         postcode = PostCode;
@@ -91,8 +88,8 @@ public class Building {
         notes = "";
         buildingType = "";
     }
-    Building(int UnitNumber, String Address, int PostCode, String Suburb, String City)
-    {
+
+    Building(int UnitNumber, String Address, int PostCode, String Suburb, String City) {
         unitNumber = UnitNumber;
         address = Address;
         postcode = PostCode;
@@ -104,8 +101,8 @@ public class Building {
         notes = "";
         buildingType = "";
     }
-    Building(int UnitNumber, String Address, int PostCode, String Suburb, String City, String State)
-    {
+
+    Building(int UnitNumber, String Address, int PostCode, String Suburb, String City, String State) {
         unitNumber = UnitNumber;
         address = Address;
         postcode = PostCode;
@@ -117,8 +114,8 @@ public class Building {
         notes = "";
         buildingType = "";
     }
-    Building(int UnitNumber, String Address, int PostCode, String Suburb, String City, String State, String BuildingName)
-    {
+
+    Building(int UnitNumber, String Address, int PostCode, String Suburb, String City, String State, String BuildingName) {
         unitNumber = UnitNumber;
         address = Address;
         postcode = PostCode;
@@ -130,8 +127,8 @@ public class Building {
         notes = "";
         buildingType = "";
     }
-    Building(int UnitNumber, String Address, int PostCode, String Suburb, String City, String State, String BuildingName, int Rooms)
-    {
+
+    Building(int UnitNumber, String Address, int PostCode, String Suburb, String City, String State, String BuildingName, int Rooms) {
         unitNumber = UnitNumber;
         address = Address;
         postcode = PostCode;
@@ -143,8 +140,8 @@ public class Building {
         notes = "";
         buildingType = "";
     }
-    Building(int UnitNumber, String Address, int PostCode, String Suburb, String City, String State, String BuildingName, int Rooms, String Notes)
-    {
+
+    Building(int UnitNumber, String Address, int PostCode, String Suburb, String City, String State, String BuildingName, int Rooms, String Notes) {
         unitNumber = UnitNumber;
         address = Address;
         postcode = PostCode;
@@ -156,8 +153,8 @@ public class Building {
         notes = Notes;
         buildingType = "";
     }
-     Building(int UnitNumber, String Address, int PostCode, String Suburb, String City, String State, String BuildingName, int Rooms, String Notes, String BuildingType)
-    {
+
+    Building(int UnitNumber, String Address, int PostCode, String Suburb, String City, String State, String BuildingName, int Rooms, String Notes, String BuildingType) {
         unitNumber = UnitNumber;
         address = Address;
         postcode = PostCode;
@@ -169,99 +166,95 @@ public class Building {
         notes = Notes;
         buildingType = BuildingType;
     }
-    
-    
+
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "Unit Number: " + unitNumber + "\n" + "Address: " + address + "\n" + "Suburb: " + suburb
                 + "\n" + "Postcode: " + postcode + "\n" + "City: " + city + "\n" + "State: " + state + "\n"
                 + "BuildingName: " + buildingName + "\n" + "Rooms: " + rooms + "\n" + "Notes: " + notes + "\n"
-                + "Building Type: " + buildingType; 
+                + "Building Type: " + buildingType;
     }
-    
+
     //setters
-    void setUnitNumber(int number)
-    {
-     unitNumber = number;   
+    void setUnitNumber(int number) {
+        unitNumber = number;
     }
-    void setAddress(String Address)
-    {
-       address = Address; 
+
+    void setAddress(String Address) {
+        address = Address;
     }
-    void setPostCode(int code)
-    {
+
+    void setPostCode(int code) {
         postcode = code;
     }
-    void setSuburb(String Suburb)
-    {
+
+    void setSuburb(String Suburb) {
         suburb = Suburb;
     }
-    void setCity(String cityName)
-    {
+
+    void setCity(String cityName) {
         city = cityName;
     }
-    void setState(String State)
-    {
+
+    void setState(String State) {
         state = State;
     }
-    void setBuildingName(String BuildingName)
-    {
+
+    void setBuildingName(String BuildingName) {
         buildingName = BuildingName;
     }
-    void setRooms(int numOfRooms)
-    {
+
+    void setRooms(int numOfRooms) {
         rooms = numOfRooms;
     }
-    void setNotes(String note)
-    {
+
+    void setNotes(String note) {
         notes = note;
     }
-    void setBuildingType(String BuildingType)
-    {
+
+    void setBuildingType(String BuildingType) {
         buildingType = BuildingType;
     }
-    
+
     //Getters
-    int setUnitNumber()
-    {
-     return unitNumber;   
+    int setUnitNumber() {
+        return unitNumber;
     }
-    String setAddress()
-    {
-       return address; 
+
+    String setAddress() {
+        return address;
     }
-    int setPostCode()
-    {
+
+    int setPostCode() {
         return postcode;
     }
-    String setSuburb()
-    {
+
+    String setSuburb() {
         return suburb;
     }
-    String setCity()
-    {
+
+    String setCity() {
         return city;
     }
-    String setState()
-    {
-       return state;
+
+    String setState() {
+        return state;
     }
-    String setBuildingName()
-    {
+
+    String setBuildingName() {
         return buildingName;
     }
-    int setRooms()
-    {
+
+    int setRooms() {
         return rooms;
     }
-    String setNotes()
-    {
+
+    String setNotes() {
         return notes;
     }
-    String setBuildingType()
-    {
+
+    String setBuildingType() {
         return buildingType;
     }
-    
+
 }
