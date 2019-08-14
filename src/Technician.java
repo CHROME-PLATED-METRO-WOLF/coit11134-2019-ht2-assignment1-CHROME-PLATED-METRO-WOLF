8dfe4110ae825ea4f96f00a0bdadb48efb356d3f
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2019-08-14 15:22:59 +1000

    Created Technician class. added basic data to class.

---
+++ src/Technician.java
@@ -0,0 +1,8 @@
+//This class will probably extend from user on assignment 2 when data is permenantly saved on disk.
+public class Technician {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private short age;
+    private String notes;
+}
