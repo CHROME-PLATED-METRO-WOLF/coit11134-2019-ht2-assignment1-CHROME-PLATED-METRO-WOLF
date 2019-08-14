4f12a9ae58fd397d8db678e725b877c04d991bf3
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2019-08-14 16:09:32 +1000

    Added constructors.

--- src/Installation.java
+++ src/Installation.java
@@ -2,4 +2,36 @@ public class Installation extends Building{
     private int horsePower;
     private int numZones;
     private int numOutlets;
+    //could have multiple technitians to one Installation
+    private Technician technician;
+    
+    //
+    Installation(Technician Technician)
+    {
+        horsePower = 0;
+        numZones = 0;
+        numOutlets = 0;
+        technician = Technician;
+    }
+    Installation(Technician Technician, int HorsePower)
+    {
+        horsePower = HorsePower;
+        numZones = 0;
+        numOutlets = 0;
+        technician = Technician;
+    }
+    Installation(Technician Technician, int HorsePower, int NumZones)
+    {
+        horsePower = HorsePower;
+        numZones = NumZones;
+        numOutlets = 0;
+        technician = Technician;
+    }
+    Installation(Technician Technician, int HorsePower, int NumZones, int NumOutlets)
+    {
+        horsePower = HorsePower;
+        numZones = NumZones;
+        numOutlets = NumOutlets;
+        technician = Technician;
+    }
 }
