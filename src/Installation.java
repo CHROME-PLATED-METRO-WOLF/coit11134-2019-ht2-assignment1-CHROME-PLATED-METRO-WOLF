c61e907a0b3c95945f5dcae3382cdc90ac334fbe
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2019-08-14 16:13:26 +1000

    Added setters

--- src/Installation.java
+++ src/Installation.java
@@ -2,10 +2,10 @@ public class Installation extends Building{
     private int horsePower;
     private int numZones;
     private int numOutlets;
-    //could have multiple technitians to one Installation
+    //Assume that only one technitian can be assigned to one installation for now.
     private Technician technician;
     
-    //
+    //Constructors
     Installation(Technician Technician)
     {
         horsePower = 0;
@@ -32,6 +32,24 @@ public class Installation extends Building{
         horsePower = HorsePower;
         numZones = NumZones;
         numOutlets = NumOutlets;
+        technician = Technician;
+    }
+    
+    //Setters
+    void setHorsePower(int HorsePower)
+    {
+       horsePower = HorsePower; 
+    }
+    void setNumZones(int numberOfZones)
+    {
+        numZones = numberOfZones;
+    }
+    void setOutlets(int numberOfOutlets)
+    {
+        numOutlets = numberOfOutlets;
+    }
+    void setTechnitian(Technician Technician)
+    {
         technician = Technician;
     }
 }
