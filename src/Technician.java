eba0bf787ef286064b9f08b7ca10a586623941aa
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2019-08-14 16:29:49 +1000

    Added constructors

--- src/Technician.java
+++ src/Technician.java
@@ -5,4 +5,54 @@ public class Technician {
     private String phoneNumber;
     private short age;
     private String notes;
+    
+    //Constructors
+    Technician()
+    {
+        firstName = "";
+        lastName = "";
+        phoneNumber = "";
+        age = 0;
+        notes = "";
+    }
+    Technician(String FirstName)
+    {
+        firstName = FirstName;
+        lastName = "";
+        phoneNumber = "";
+        age = 0;
+        notes = "";
+    }
+    Technician(String FirstName, String LastName)
+    {
+        firstName = FirstName;
+        lastName = LastName;
+        phoneNumber = "";
+        age = 0;
+        notes = "";
+    }
+    Technician(String FirstName, String LastName, String PhoneNumber)
+    {
+        firstName = FirstName;
+        lastName = LastName;
+        phoneNumber = PhoneNumber;
+        age = 0;
+        notes = "";
+    }
+    Technician(String FirstName, String LastName, String PhoneNumber, short Age)
+    {
+        firstName = FirstName;
+        lastName = LastName;
+        phoneNumber = PhoneNumber;
+        age = Age;
+        notes = "";
+    }
+    Technician(String FirstName, String LastName, String PhoneNumber, short Age, String Notes)
+    {
+        firstName = FirstName;
+        lastName = LastName;
+        phoneNumber = PhoneNumber;
+        age = Age;
+        notes = Notes;
+    }
 }
