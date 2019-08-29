055ded1fc1bc991d146628017a05b6cc53a72838
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2019-08-30 00:20:10 +1000

    Added highrise sub class

---
+++ src/Highrise.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Terry
+ */
+public class Highrise extends Building {
+    int numOfFloors;
+    
+    Highrise()
+    {
+      numOfFloors = 0;  
+    }
+    Highrise(int NumOfFloors)
+    {
+      numOfFloors = NumOfFloors;  
+    }
+        
+    void setNumOfFloors(int NumOfFloors)
+    {
+        numOfFloors = NumOfFloors; 
+    }
+    
+    int getNumOfFloors()
+    {
+        return numOfFloors; 
+    }
+}
