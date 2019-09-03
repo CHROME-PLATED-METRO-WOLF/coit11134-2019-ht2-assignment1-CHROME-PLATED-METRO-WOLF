22145365b85accb9f6f239d988768f191b43d286
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2019-09-03 15:51:52 +1000

    Added author header.

--- src/Highrise.java
+++ src/Highrise.java
@@ -1,12 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Terry
+ Created by Caleb Davidson for assignment 2 of Object Oriented Programming
  */
 public class Highrise extends Building {
 
